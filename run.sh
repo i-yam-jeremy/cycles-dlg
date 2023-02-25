3b86e501070f78ba5d218175956c86eaf6fd0451
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2023-02-25 14:55:34 -0800

    Backup

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 unset CYCLES_CUDA_ADAPTIVE_COMPILE
-build/Release/bin/cycles --samples 100 --device OPTIX --output image.png "/media/jeremy/DRIVE/home/jeremy/Documents/alab-v2.0.1/ALab/entry.usda" --width 1024 --height 512
+export ASAN_OPTIONS=halt_on_error=0
+build/Debug/bin/cycles --samples 100 --device OPTIX --output image.png "test.usda" --width 1024 --height 512
